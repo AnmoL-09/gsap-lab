bbaecaa1b5ef7c9d21b799e5c1ab39da3bb7928a
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-05 00:30:22 +0530

    add gsap timeline

--- Basics of GSAP/script.js
+++ Basics of GSAP/script.js
@@ -1,21 +1,43 @@
-gsap.to(".box1", {
+// Traditional method without using timeline
+
+// gsap.to(".box1", {
+//   x: 1500,
+//   rotate: 360,
+//   duration: 1.5,
+//   delay: 1,
+// });
+
+// gsap.to(".box2", {
+//   x: 1500,
+//   backgroundColor: "yellow",
+//   duration: 1.5,
+//   delay: 2.5,
+// });
+
+// gsap.to(".box3", {
+//   x: 1500,
+//   scale: 0.5,
+//   borderRadius: "50%",
+//   duration: 1.5,
+//   delay: 4,
+// });
+
+// Using timeline
+var tl = gsap.timeline();
+
+tl.to(".box1", {
   x: 1500,
   rotate: 360,
   duration: 1.5,
   delay: 1,
 });
 
-gsap.to(".box2", {
+tl.to(".box2", {
   x: 1500,
-  backgroundColor: "yellow",
   duration: 1.5,
-  delay: 2.5,
 });
 
-gsap.to(".box3", {
+tl.to(".box3", {
   x: 1500,
-  scale: 0.5,
-  borderRadius: "50%",
   duration: 1.5,
-  delay: 4,
 });
