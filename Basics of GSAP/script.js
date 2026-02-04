8e2af551c85d6118700d095273533663d4c3e326
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-05 00:21:58 +0530

    traditional technique without using timeline

--- Basics of GSAP/script.js
+++ Basics of GSAP/script.js
@@ -1,8 +1,21 @@
-gsap.to(".box", {
-  x: 1200,
-  duration: 1,
-  delay: 1,
+gsap.to(".box1", {
+  x: 1500,
   rotate: 360,
-  repeat: -1,
-  yoyo: true,
+  duration: 1.5,
+  delay: 1,
+});
+
+gsap.to(".box2", {
+  x: 1500,
+  backgroundColor: "yellow",
+  duration: 1.5,
+  delay: 2.5,
+});
+
+gsap.to(".box3", {
+  x: 1500,
+  scale: 0.5,
+  borderRadius: "50%",
+  duration: 1.5,
+  delay: 4,
 });
