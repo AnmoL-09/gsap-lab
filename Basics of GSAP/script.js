3815e41dda4e4cabad7c83fc3cab3bcbb2a9adef
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-05 00:15:24 +0530

    add repeat animation ( yoyo )

--- Basics of GSAP/script.js
+++ Basics of GSAP/script.js
@@ -1,7 +1,8 @@
-gsap.from("h1", {
-  opacity: 0,
-  duration: 2,
-  y: 30,
+gsap.to(".box", {
+  x: 1200,
+  duration: 1,
   delay: 1,
-  stagger: 1,
+  rotate: 360,
+  repeat: -1,
+  yoyo: true,
 });
