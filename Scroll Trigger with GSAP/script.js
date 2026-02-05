d18093df1a554697d252fc534a837761475eddc6
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-05 20:24:34 +0530

    add pin property

--- Scroll Trigger with GSAP/script.js
+++ Scroll Trigger with GSAP/script.js
@@ -1,14 +1,12 @@
-gsap.from(".page2 .box", {
-  scale: 0,
-  opacity: 0,
-  rotate: 720,
-  duration: 1,
+gsap.to(".page2 h1", {
+  transform: "translateX(-150%)",
   scrollTrigger: {
-    trigger: ".page2 .box",
+    trigger: ".page2",
     scroller: "body",
     markers: true,
-    start: "top 60%",
-    end: "top 30%",
+    start: "top 0%",
+    end: "top -140%",
     scrub: 2,
+    pin: true,
   },
 });
