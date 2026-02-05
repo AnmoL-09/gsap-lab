f216c38bcccb1a64b3dede52097799a2a2c82bc3
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-05 18:48:35 +0530

    add simple scroll trigger animation

--- Scroll Trigger with GSAP/script.js
+++ Scroll Trigger with GSAP/script.js
@@ -1,22 +1,22 @@
-gsap.from(".page1 .box", {
-  scale: 0,
-  delay: 1,
+gsap.from(".page2 h1", {
+  opacity: "0",
   duration: 2,
-  rotate: 360,
+  x: 500,
+  scrollTrigger: {
+    trigger: ".page2 h1",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
 });
-
-gsap.from(".page2 .box", {
-  scale: 0,
-  delay: 1,
+gsap.from(".page2 h2", {
+  opacity: "0",
   duration: 2,
-  rotate: 360,
-  scrollTrigger: ".page2 .box",
-});
-
-gsap.from(".page3 .box", {
-  scale: 0,
-  delay: 1,
-  duration: 2,
-  rotate: 360,
-  scrollTrigger: ".page3 .box",
+  x: -500,
+  scrollTrigger: {
+    trigger: ".page2 h2",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
 });
