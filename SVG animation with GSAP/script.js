2eb9462e5ff313090209c8261d5a388a12751289
AnmoL-09 <anmolmahobiya092@gmail.com>
2026-02-06 16:44:33 +0530

    add animation to curve

---
+++ SVG animation with GSAP/script.js
@@ -0,0 +1,11 @@
+var path = "M 10 100 Q 500 100 990 100";
+var finalPath = "M 10 100 Q 500 100 990 100";
+
+var string = document.querySelector(".string");
+
+string.addEventListener("mousemove", function (dets) {
+  path = `M 10 100 Q 500 ${dets.y} 990 100`;
+  gsap.to("svg path", {
+    attr: { d: path },
+  });
+});
